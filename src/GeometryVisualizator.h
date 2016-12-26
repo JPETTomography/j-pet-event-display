6f21c23d23c38df3208058ba12d6d738b09ca7e0
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-12-26 16:08:43 +0100

    Remove @file doxygen command cause it is not used anyway

--- src/GeometryVisualizator.h
+++ src/GeometryVisualizator.h
@@ -10,7 +10,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  @file GeometryVisualizator.h
  *  @brief Class visualizes geometry along with fired strips.
  */
 
