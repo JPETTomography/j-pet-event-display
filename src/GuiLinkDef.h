adfb71fa558e73778f0a7759d9588d27df1246cd
Grey <grey277@wp.pl>
2017-02-15 12:15:26 +0100

    Add LinkDef file

--- src/GuiLinkDef.h
+++ src/GuiLinkDef.h
@@ -1,2 +1,11 @@
-#pragma link C++ class GuiSignalController;
-#pragma link C++ class GuiController;
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+
+#pragma link C++ class jpet_event_display::GuiController;
+#pragma link C++ class jpet_event_display::GuiSignalController;
+
+#endif
