8f937b6242930540f938f60e15273c87538aa71c
Grey <grey277@wp.pl>
2017-03-14 20:14:11 +0100

    Remove comments

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
  *
  */
 
-//#include <boost/program_options.hpp> /// for command line option parsing
 #include <TRint.h>
 #include <iostream>
 #include "src/EventDisplay.h"
@@ -20,30 +19,6 @@
 int main(int argc, char** argv)
 {
   using namespace jpet_event_display;
-  /*namespace po = boost::program_options;
-
-  std::string inFile = "";
-  std::string fileType = "JPetTimeWindow";
-
-  try {
-    po::options_description desc("Allowed options");
-    desc.add_options()
-    ("help,h", "produce help message")
-    ("input,i", po::value(&inFile), "Input file")
-    ("type,t", po::value(&fileType), "type input file type"); /// maybe it is not necessary
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-    if (vm.count("help")) {
-      std::cout << desc << "\n";
-      return 1;
-    }
-  } catch (std::exception& e) {
-    std::cout << e.what() << "\n";
-    return 1;
-  }*/
-
   EventDisplay myDisplay;
-
   return 0;
 }
